6d4e2c16a89327c5e9153ee378cd4fd5b3cb87a7
Ant0yne <antoine.soliv@gmail.com>
2024-06-21 11:07:50 +0200

    favicon + app name + fix overflowing text in album in Profile page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<footer className="text-center font-bold px-1 text-slate-900 border-t-2 py-1 fixed inset-x-0 bottom-0 bg-white">
-			<small>Made by Antoine SOLIVERES - djob Frontend Technical Test</small>
+			<small>Made by Antoine SOLIVERES - Frontend Technical Test</small>
 		</footer>
 	);
 };
