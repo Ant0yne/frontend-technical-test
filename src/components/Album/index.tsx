9029f689a2b772c23fc1ba94389d115e1acfdd4a
Ant0yne <antoine.soliv@gmail.com>
2024-06-21 11:09:56 +0200

    padding on album name in Profile page

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -5,7 +5,7 @@ const Album = ({ title, id }: { title: string; id: number }) => {
 	return (
 		<Link to={`/photos/${id}`}>
 			<nav className="w-[150px] h-[150px] m-3 bg-slate-400 rounded-md relative hover:opacity-75 hover:border-2 border-slate-200">
-				<div className="h-[105px] lg:h-[100px] overflow-hidden">
+				<div className="h-[105px] lg:h-[100px] overflow-hidden px-2">
 					<p className="text-center italic pt-3 ">{title}</p>
 				</div>
 				<Button className="bg-slate-900 lg:text-xl lg:py-5 absolute bottom-0 w-full">
