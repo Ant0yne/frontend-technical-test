8d0404c120705706cc5b7423743b428756c95b4f
Ant0yne <antoine.soliv@gmail.com>
2024-06-21 09:20:48 +0200

    fix import error for build

--- src/components/TitleH2/index.tsx
+++ src/components/TitleH2/index.tsx
@@ -1,5 +1,3 @@
-import { PropsWithChildren } from "react";
-
 const TitleH2 = ({ title }: { title: string | undefined }) => {
 	return (
 		<h2 className="text-center text-xl font-bold my-4 text-slate-900 lg:text-3xl">
